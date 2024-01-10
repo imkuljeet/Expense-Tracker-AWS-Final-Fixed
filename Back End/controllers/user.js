0bbebd03c324642c4505bbdedb953cec671aedea
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2024-01-10 15:02:03 +0530

    Your Password is at risk!!
    Used bcrypt.. to encrypt the password.. and then used bcrypt.compare to compare it with the db password

--- Back End/controllers/user.js
+++ Back End/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/users');
+const bcrypt = require("bcrypt");
 
 function isStringInvalid(string) {
   if (string === undefined || string.length === 0) {
@@ -11,7 +12,6 @@ function isStringInvalid(string) {
 const signup = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    console.log("email is", email);
 
     if (
       isStringInvalid(name) ||
@@ -23,8 +23,14 @@ const signup = async (req, res) => {
         .json({ err: "Bad Parameters, Something is missing" });
     }
 
-    await User.create({ name, email, password });
-    res.status(201).json({ message: "Successfully Created New User" });
+    const saltrounds = 10;
+    bcrypt.hash(password, saltrounds, async (err, hash) => {
+      console.log(err);
+
+      await User.create({ name, email, password: hash });
+      res.status(201).json({ message: "Successfully create New user" });
+    });
+
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal server error" });
@@ -36,21 +42,26 @@ const login = async (req, res) => {
     const { email, password } = req.body;
 
     if (isStringInvalid(email) || isStringInvalid(password)) {
-      return res.status(400).json({ success: false, message: 'Email or password is missing' });
+      return res.status(400).json({ success: false, message: "Email or password is missing" });
     }
 
-    console.log(password);
+    console.log("Password is -- grabbed from front end>>>>>",password);
 
-    const user = await User.findAll({ where: { email } });
+    const user = await User.findAll({ where: { email } }); //same as User.findAll({ where: { email: email } });
 
     if (user.length > 0) {
-      if (user[0].password === password) {
-        return res.status(200).json({ success: true, message: "User logged in successfully" });
-      } else {
-        return res.status(400).json({ success: false, message: 'Password is incorrect' });
-      }
+      bcrypt.compare(password, user[0].password, (err, result) => {
+        if (err) {
+          throw new Error("Something went wrong");
+        }
+        if (result === true) {
+          return res.status(200).json({ success: true, message: "User logged in successfully" });
+        } else {
+          return res.status(400).json({ success: false, message: "Password is incorrect" });
+        }
+      });
     } else {
-      return res.status(404).json({ success: false, message: 'User does not exist' });
+      return res.status(404).json({ success: false, message: "User Doesnot exitst" });
     }
   } catch (err) {
     return res.status(500).json({ success: false, message: err.message });
@@ -61,3 +72,6 @@ module.exports = {
     signup,
     login
 }
+
+
+
