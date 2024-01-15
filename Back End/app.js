b85c239e8484134457169631762d05839e23880d
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2024-01-15 11:01:07 +0530

    Lets Go Live
    Helmet And Morgan added.... Logs of all requests saved as a file

--- Back End/app.js
+++ Back End/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
 
 const sequelize = require('./util/database');
 
@@ -19,17 +21,24 @@ const resetPasswordRoutes = require('./routes/resetpassword')
 const app = express();
 
 const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
 dotenv.config();
+const accessLogStream = fs.createWriteStream(path.join(__dirname,'access.log'),{flags: 'a'});
 
 app.use(cors());
 
 app.use(express.json());  
+app.use(morgan('combined',{stream: accessLogStream}));
 
 app.use('/user',userRoutes);
 app.use('/expense',expenseRoutes);
 app.use('/purchase', purchaseRoutes);
 app.use('/premium', premiumFeatureRoutes);
 app.use('/password', resetPasswordRoutes);
+
+app.use(helmet());
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
@@ -48,7 +57,8 @@ DownloadHistory.belongsTo(User);
 sequelize
 .sync()
 .then((result)=>{
-    app.listen(3000);
+    app.listen(process.env.PORT);
+    console.log("SERVER is running at",process.env.PORT);
 })
 .catch((err)=>{
     console.log(err);
